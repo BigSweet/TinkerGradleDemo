a2648ece0bae3f33716d6c9766fb3864e2a12c00
sunwentao <sunwentao@primedu.cn>
2017-10-12 11:28:40 +0800

    tinker

---
+++ app/src/main/java/com/anlaiye/swt/gradletest/MainActivity.java
@@ -0,0 +1,96 @@
+package com.anlaiye.swt.gradletest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    //加载补丁
+    public void loadPath(View view) {
+        requestPermi();
+    }
+
+    public void loadApk() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk";
+        File file = new File(path);
+        if (file.exists()) {
+            Toast.makeText(this, "补丁已经存在", Toast.LENGTH_SHORT).show();
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), path);
+            Log.d("swy", path);
+        } else {
+            Toast.makeText(this, "补丁已经不存在", Toast.LENGTH_SHORT).show();
+            Log.d("swy", path);
+        }
+    }
+
+    private void requestPermi() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                Log.d("swt", "requestPermi: ");
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            //有权限直接加载apk
+            loadApk();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    //权限申请成功加载apk
+                    loadApk();
+                    Log.d("swt", "permissionsuss: ");
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/anlaiye/swt/gradletest/SimpleTinkerInApplicationLike.java
@@ -0,0 +1,33 @@
+package com.anlaiye.swt.gradletest;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.support.multidex.MultiDex;
+
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+@DefaultLifeCycle(application = ".SimpleTinkerInApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag =true)
+public class SimpleTinkerInApplicationLike extends ApplicationLike {
+    public SimpleTinkerInApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        MultiDex.install(base);
+        TinkerInstaller.install(this);
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
