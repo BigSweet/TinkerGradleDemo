5027a7a722c898387a31c26b8a68f90abd2f9ae7
sunwentao <wentao.sun@yintech.com>
2021-04-29 16:12:41 +0800

    add 更新最新的tinker

--- app/src/main/java/com/anlaiye/swt/gradletest/MainActivity.java
+++ app/src/main/java/com/anlaiye/swt/gradletest/MainActivity.java
@@ -2,14 +2,16 @@ package com.anlaiye.swt.gradletest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
@@ -23,7 +25,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+    }
 
+    private boolean hasRequiredPermissions() {
+        if (Build.VERSION.SDK_INT >= 16) {
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        } else {
+            // When SDK_INT is below 16, READ_EXTERNAL_STORAGE will also be granted if WRITE_EXTERNAL_STORAGE is granted.
+            final int res = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return res == PackageManager.PERMISSION_GRANTED;
+        }
     }
 
     //加载补丁
--- app/src/main/java/com/anlaiye/swt/gradletest/SimpleTinkerInApplicationLike.java
+++ app/src/main/java/com/anlaiye/swt/gradletest/SimpleTinkerInApplicationLike.java
@@ -1,19 +1,23 @@
 package com.anlaiye.swt.gradletest;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.support.multidex.MultiDex;
+import android.os.Build;
+
+import androidx.multidex.MultiDex;
 
 import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.entry.ApplicationLike;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
-import com.tencent.tinker.loader.app.ApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
-@DefaultLifeCycle(application = ".SimpleTinkerInApplication",
+@DefaultLifeCycle(application = "tinker.sample.android.app.SampleApplication",
         flags = ShareConstants.TINKER_ENABLE_ALL,
-        loadVerifyFlag =true)
+        loadVerifyFlag = false)
 public class SimpleTinkerInApplicationLike extends ApplicationLike {
+
     public SimpleTinkerInApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
     }
@@ -26,8 +30,8 @@ public class SimpleTinkerInApplicationLike extends ApplicationLike {
 
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
     }
 }
